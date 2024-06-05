5d38aef74514834b21862fa58391c3f25b7f4246
Hilal-Bilun-Kara <bilunkara@gmail.com>
2024-06-05 17:39:03 +0300

    S15D1 Done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import org.example.mobile.Contact;
+import org.example.mobile.MobilePhone;
+import org.example.models.Grocery;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        /*Grocery grocery = new Grocery();
+        grocery.startGrocery();*/
 
+
+        System.out.println("******");
+
+        MobilePhone mobilePhone = new MobilePhone("045747574",new ArrayList<>());
+        mobilePhone.addNewContact(new Contact("Bilun","45435443546"));
+        mobilePhone.addNewContact(new Contact("Bilun",null));
+        mobilePhone.addNewContact(new Contact("Hilal","45435443243"));
+        mobilePhone.printContacts();
     }
 }
